ce34a2a0f4d0b263f830d5588b18a260568c4b1a
Dominicmburu <dominicmburu034@gmail.com>
2024-07-12 12:54:05 +0300

    updated v4

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.nav-link');
 
     navLinks.forEach(link => {
-        if (link.href === currentLocation || (link.id === 'nav-home' && currentLocation.endsWith('index.html'))) {
+        if (link.href === currentLocation || (link.id === 'nav-home')) {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
